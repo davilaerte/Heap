d98e4acd91e5393f5a5f6ad69aa50eba90c3be1b
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-07-25 19:26:17 -0300

    Implement heap

--- heap.hs
+++ heap.hs
@@ -5,16 +5,50 @@ swap lst i j = swapAux lst 0
                                | index == j = lst !! i : swapAux as (index+1)
                                | otherwise = a : swapAux as (index+1)
 
-data MinHeap a = MinHeap [a] deriving (Eq, Show)
+parent :: Int -> Int
+parent x = div (x-1) 2
 
-class Heap a where
-    buildHeap :: a -> a
-    heapFy :: int -> a -> a
-    extract :: a -> b
+left :: Int -> Int
+left x = (2*x) + 1
 
--- data MinHeap a = MinHeap [a] deriving (Eq, Show)
+right :: Int -> Int
+right x = (x + 1) * 2
 
--- instance Heap (MinHeap a) where
---     buildHeap (MinHeap x) = MinHeap [y | y <- x, False]
---     heapFy x heap = heap
---     extract (MinHeap x) = 1
+class (Ord a) => Heap a where
+    buildHeap :: (a -> a -> Bool) -> [a] -> [a]
+    heapFy :: Int -> (a -> a -> Bool) -> [a] -> [a]
+    extract :: [a] -> (a -> a -> Bool) -> (a, [a])
+    insert :: (a -> a -> Bool) -> a -> [a] -> [a]
+
+    heapFy i compare heap | i /= maior = heapFy maior compare (swap heap i heapFyAux)
+                          | otherwise = heap
+        where dir = right i
+              esq = left i
+              len = length heap
+              maior = heapFyAux
+              heapFyAux | esq >= len = i
+                        | dir >= len  = if (heap !! esq) `compare` (heap !! i) then esq else i
+                        | (heap !! esq) `compare` (heap !! i) && (heap !! esq) `compare` (heap !! dir) = esq
+                        | (heap !! dir) `compare` (heap !! i) && (heap !! dir) `compare` (heap !! esq) = dir
+                        | otherwise = i
+    
+    buildHeap compare [] = []
+    buildHeap compare heap = buildAux pai heap
+        where pai = parent ((length heap) - 1)
+              buildAux 0 heapAux = heapFy 0 compare heapAux
+              buildAux i heapAux = buildAux (i-1) (heapFy i compare heapAux)
+    
+    extract (a:as) compare = (a, (heapFy 0 compare ((last as):(init as))))
+
+    insert compare element heap = insertAux (heap ++ [element]) pos
+        where pos = (length (heap ++ [element]) - 1)
+              insertAux heapAux 0 = heapAux
+              insertAux heapAux i | (heapAux !! i) `compare` (heapAux !! (parent i)) = insertAux (swap heapAux (parent i) i) (parent i)
+                                  | otherwise = heapAux 
+
+
+
+instance Heap Integer where
+
+instance Heap Char where
+                        
