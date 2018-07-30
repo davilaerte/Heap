2f57e2a33cf62d63f11364971f6d8a51af830721
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-07-30 03:50:08 -0300

    Added endpoints for heap methods

--- heap/app/Main.hs
+++ heap/app/Main.hs
@@ -4,32 +4,82 @@ module Main where
 import Web.Scotty
 import Data.Monoid ((<>))
 import Data.Text
-import HeapModule (swap, Heap(..), Comparator(..), Comparable(..), buildHeap)
-import Data.Aeson (ToJSON, toJSON, (.=), object)
+import HeapModule (swap, Heap(..), Comparator(..), Comparable(..), buildHeap, insert, extract, extracts, heapSort)
+import Data.Aeson (FromJSON(..), ToJSON(..), Value(..), (.=), (.:), object)
 
-data Teste = Teste {x :: [Char], y :: Int}
+-- Funcoes auxiliares
+getComparator :: (Comparable a) => Bool -> (a -> a -> Bool)
+getComparator True = compareMax
+getComparator False = compareMin
 
-instance ToJSON Teste where
-  toJSON (Teste x y) = object ["x" .= x, "y" .= y]
+-- Tipos e instancias auxiliares
+data HeapAux = HeapAux {comp :: Bool, heapLis :: [Integer]}
 
-instance ToJSON (Heap m) where
-  toJSON heap = object ["heap" .= (heapList heap)]
+instance (ToJSON m) => ToJSON (Heap m) where
+  toJSON (Heap comp heap) = object ["heap" .= heap]
 
+instance FromJSON HeapAux where
+    parseJSON (Object v) = HeapAux
+        <$> v .: "comp"
+        <*> v .: "heapLis"
+        
+-- rotas (endpoints)
 routes :: ScottyM ()
 routes = do
-  get "/hello" $ do
-    json (toJSON (buildHeap (Comparator compareMax) [1..10]))
-  get "/teste/:name" $ do
-    name <- param "name"
+  post "/insert/:elem" $ do
+    elem <- param "elem"
+    heapAux <- jsonData
+    let heap = Heap (Comparator (getComparator (comp heapAux))) (heapLis heapAux)
     addHeader "Access-Control-Allow-Origin" "http://localhost:8000"
     addHeader "Access-Control-Expose-Headers" "APP_VERSION"
     addHeader "Access-Control-Allow-Headers" "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
     addHeader "Access-Control-Allow-Methods" "POST, GET, PUT, DELETE, PATCH"
     addHeader "Content-Type" "application/json; charset=utf-8"
     addHeader "APP_VERSION" "1.001"
-    text ("OLA EU sou" <> name <> "!")
+    json (toJSON (insert heap elem))
+  post "/extract" $ do
+    heapAux <- jsonData
+    let heap = Heap (Comparator (getComparator (comp heapAux))) (heapLis heapAux)
+    addHeader "Access-Control-Allow-Origin" "http://localhost:8000"
+    addHeader "Access-Control-Expose-Headers" "APP_VERSION"
+    addHeader "Access-Control-Allow-Headers" "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
+    addHeader "Access-Control-Allow-Methods" "POST, GET, PUT, DELETE, PATCH"
+    addHeader "Content-Type" "application/json; charset=utf-8"
+    addHeader "APP_VERSION" "1.001"
+    json (toJSON (extract heap))
+  post "/extracts" $ do
+    heapAux <- jsonData
+    let heap = Heap (Comparator (getComparator (comp heapAux))) (heapLis heapAux)  
+    addHeader "Access-Control-Allow-Origin" "http://localhost:8000"
+    addHeader "Access-Control-Expose-Headers" "APP_VERSION"
+    addHeader "Access-Control-Allow-Headers" "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
+    addHeader "Access-Control-Allow-Methods" "POST, GET, PUT, DELETE, PATCH"
+    addHeader "Content-Type" "application/json; charset=utf-8"
+    addHeader "APP_VERSION" "1.001"
+    json (toJSON (extracts heap))
+  post "/build" $ do
+    heapAux <- jsonData
+    let comparator = Comparator (getComparator (comp heapAux))
+    let heapLista = heapLis heapAux
+    addHeader "Access-Control-Allow-Origin" "http://localhost:8000"
+    addHeader "Access-Control-Expose-Headers" "APP_VERSION"
+    addHeader "Access-Control-Allow-Headers" "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
+    addHeader "Access-Control-Allow-Methods" "POST, GET, PUT, DELETE, PATCH"
+    addHeader "Content-Type" "application/json; charset=utf-8"
+    addHeader "APP_VERSION" "1.001"
+    json (toJSON (buildHeap comparator heapLista))
+  post "/sort" $ do
+    heapAux <- jsonData
+    let heapLista = heapLis heapAux
+    let comparator = Comparator (getComparator (comp heapAux))  
+    addHeader "Access-Control-Allow-Origin" "http://localhost:8000"
+    addHeader "Access-Control-Expose-Headers" "APP_VERSION"
+    addHeader "Access-Control-Allow-Headers" "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"
+    addHeader "Access-Control-Allow-Methods" "POST, GET, PUT, DELETE, PATCH"
+    addHeader "Content-Type" "application/json; charset=utf-8"
+    addHeader "APP_VERSION" "1.001"
+    json (toJSON (heapSort heapLista comparator))
 
 main = do
   putStrLn "Iniciando server"
-  print (swap [1..10] 1 5)
   scotty 3000 routes
